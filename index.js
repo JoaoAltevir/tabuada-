bf18e1940f4c7dd3f25f463236bee5e9ba1a52be
Neto <j.altevirneto@gmail.com>
2024-06-13 10:54:27 -0300

    fix: codigo escalavel

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let tabuada = 0;
+let resposta;
 
 console.log("Você deseja ver a tabuada de qual número?")
 process.stdin.on("data", function (data){
-    let entradaUsuario = Number(data.toString().trim());
-    for(let i = 0; i < 11; i++){
-        tabuada = entradaUsuario * i;
-        console.log(entradaUsuario ,"X", i, "=", tabuada);
+    let input = data.toString().trim();
+    if(!resposta){
+        resposta = Number(input);
+        for(let i = 0; i < 11; i++){
+            tabuada = resposta * i;
+            console.log(resposta ,"X", i, "=", tabuada);
+        }
     }
     process.exit();
 })
